21af0e34dc25058761c35ad38ccdb93138418beb
BobNies <elsereturn@gmail.com>
2021-09-07 06:58:05 -0400

    service scene

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+using UnityEngine;
+
+public class ButtonHandler : Button
+{
+    private string switchName;
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        Debug.Log("Down");
+        sendEvent(1);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        base.OnPointerUp(eventData);
+       // Debug.Log("Up");
+       // sendEvent(0);
+    }
+
+    public void SendBCPSwitch(string switchName)
+    {
+        this.switchName = switchName;
+    }
+
+    private void sendEvent(int switchState)
+    {
+        if (!String.IsNullOrEmpty(switchName))
+            BcpServer.Instance.Send(BcpMessage.TriggerMessage(switchName));
+        //BcpServer.Instance.Send(BcpMessage.TriggerMessage(switchName));
+        //BcpServer.Instance.Send(BcpMessage.SwitchMessage(switchName, switchState));
+    }
+}
