8c83f4364d8169e202de3a554e001a40cd31f8fe
BobNies <elsereturn@gmail.com>
2021-10-21 22:55:53 -0400

    added managers

---
+++ Assets/TiltManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Handles tilt & tilt warning
+public class TiltManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BcpMessageController.OnTiltWarning += TiltWarning;
+        BcpMessageController.OnTilt += Tilt;
+        BcpMessageController.OnSlamTilt += SlamTilt;
+    }
+
+    void OnDisable()
+    {
+        BcpMessageController.OnTiltWarning -= TiltWarning;
+        BcpMessageController.OnTilt -= Tilt;
+        BcpMessageController.OnSlamTilt -= SlamTilt;
+    }
+
+    public void TiltWarning(object sender, TiltWarningMessageEventArgs e)
+    {
+        // TODO
+        //if (!warnings.IsNone)
+          //  warnings.Value = e.Warnings;
+
+        //if (!warningsRemaining.IsNone)
+          //  warningsRemaining.Value = e.WarningsRemaining;
+    }
+
+    public void Tilt(object sender, BcpMessageEventArgs e)
+    {
+        // TODO
+    }
+
+    public void SlamTilt(object sender, BcpMessageEventArgs e)
+    {
+        // TODO
+    }
+}
