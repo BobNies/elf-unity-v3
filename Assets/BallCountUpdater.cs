80a180ac2ca9566417c02f7bd354803499004172
BobNies <elsereturn@gmail.com>
2022-03-06 14:27:58 -0500

    ball updater

--- Assets/BallCountUpdater.cs
+++ Assets/BallCountUpdater.cs
@@ -9,12 +9,13 @@ public class BallCountUpdater : MonoBehaviour
     [SerializeField] 
     public Modular3DText modular3DText = null;
     private int currentBallNo = 0;
+    private bool isShowing = false;
     private Vector3 vec = new Vector3(.2f, 1f, 1f);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        tweenIn();
     }
 
     // Update is called once per frame
@@ -23,6 +24,12 @@ public class BallCountUpdater : MonoBehaviour
         int ballNo = Globals.ballNumber;
         if (currentBallNo != ballNo)
         {
+            if(!isShowing)
+            {
+                isShowing = true;
+                //bring text into view
+                tweenIn();
+            }
             currentBallNo = ballNo;
             // Only update when it changes.
             modular3DText.Text = "Ball " + ballNo + " of 3";
@@ -30,4 +37,17 @@ public class BallCountUpdater : MonoBehaviour
             modular3DText.transform.DOShakeScale(1, vec, 10, 90f, true).SetEase(Ease.InOutFlash);
         }
     }
+
+    private void tweenIn()
+    {
+         Vector3 v = new Vector3(.5f, -0.7399979f, 1.2f);
+         modular3DText.transform.DOLocalMove(v, 1f).SetEase(Ease.InElastic);
+    }
+
+    private void tweenOut()
+    {
+        Vector3 v = new Vector3(.5f, -1f, 1.2f);
+        modular3DText.transform.DOLocalMove(v, 1f, true).SetEase(Ease.OutElastic);
+    }
+
 }
