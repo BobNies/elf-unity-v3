aa92da3910e3cc503e2e2c8476098c04fc4a36d0
BobNies <elsereturn@gmail.com>
2022-03-10 22:52:43 -0500

    book flipping done

--- Assets/BookFlip.cs
+++ Assets/BookFlip.cs
@@ -10,11 +10,18 @@ public class BookFlip : MonoBehaviour
     public float stateAnimationTime = 1f;
     public EndlessBook.PageTurnTimeTypeEnum turnTimeType = EndlessBook.PageTurnTimeTypeEnum.TotalTurnTime;
     public float turnTime = 1f;
+    public float timeBetweenTurn = 20f;
+
+    private float timer = 0f;
+    private float origTimeBetweenTurn = 20;
+    private int currentPage = 1;
+    private int cycles = 0;
 
     void Awake()
     {
         // cache the book
         book = GameObject.Find("Book").GetComponent<EndlessBook>();
+        origTimeBetweenTurn = timeBetweenTurn;
     }
 
     // Start is called before the first frame update
@@ -23,7 +30,7 @@ public class BookFlip : MonoBehaviour
         //book.TurnForward(1);
         //SetState(EndlessBook.StateEnum.OpenFront);
         //book.TurnForward(1);
-        book.TurnToPage(3, turnTimeType, turnTime);
+        //book.TurnToPage(3, turnTimeType, turnTime);
     }
 
     protected virtual void SetState(EndlessBook.StateEnum state)
@@ -38,11 +45,23 @@ public class BookFlip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            //flip the page
+            timer = origTimeBetweenTurn;
+            currentPage += 1;
+            if (currentPage > book.LastPageNumber)
+            {
+                currentPage = 1;
+                cycles += 1;
+            }
+            book.TurnToPage(currentPage, turnTimeType, turnTime);
+        }
     }
 
     private void changePage()
     {
-
+        book.TurnToPage(3, turnTimeType, turnTime);
     }
 }
