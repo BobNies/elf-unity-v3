d3728314e419cda8cfca19bff6a0fb8b68122f01
BobNies <elsereturn@gmail.com>
2022-03-15 18:36:37 -0400

    champion

--- Assets/Champion.cs
+++ Assets/Champion.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using MText;
 
@@ -7,24 +8,56 @@ public class Champion : MonoBehaviour
 {
 
     public Modular3DText name;
+    private bool updated = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        UpdateName();
+        //register to listen for new high score
+        BcpServer.Instance.Send(BcpMessage.RegisterTriggerMessage("high_score_award_display"));
+
+        //UpdateName();
+        name.Text = "aaa";  //Globals.championName;
+    }
+
+    void OnDisable()
+    {
+        BcpMessageController.OnTrigger -= Trigger;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!updated)
+        {
+            string cn = Globals.championName;
+            if (!String.IsNullOrEmpty(cn))
+            {
+                updated = true;
+                name.Text = Globals.championName;
+            }
+        }
         
     }
 
-    IEnumerator UpdateName()
-    {
-        yield return new WaitForSeconds(3);
-
-        name.Text = "aaa"; // Globals.championName;
+    public void Trigger(object sender, TriggerMessageEventArgs e)
+    {        
+        if (e.Name == "high_score_award_display")
+        {
+            try
+            {
+                string playerName = e.BcpMessage.Parameters["player_name"].Value;
+                if (!String.IsNullOrEmpty(playerName))
+                {
+                    Globals.championName = playerName;
+                    name.Text = playerName;
+                }
+            }
+            catch (Exception ex)
+            {
+                BcpServer.Instance.Send(BcpMessage.ErrorMessage("An error occurred while processing a 'high_score_award_display' trigger message: " + ex.Message, e.BcpMessage.RawMessage));
+            }
+        }
     }
 
 }
