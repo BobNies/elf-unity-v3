c0abf22a9d108f7a18939ef8b8ca3d2e04375c56
BobNies <elsereturn@gmail.com>
2022-03-15 23:13:59 -0400

    L2 screen

--- Assets/ModeManager.cs
+++ Assets/ModeManager.cs
@@ -9,6 +9,8 @@ using UnityEngine.Video;
 public class ModeManager : MonoBehaviour
 {
     public VideoManager videoManager;
+    public PlayfieldManager playfieldManager;
+
     public VideoClip videoCandyCaneForest;
     public VideoClip videoCentralPark;
     public VideoClip videoCoffee;
@@ -44,35 +46,46 @@ public class ModeManager : MonoBehaviour
 
         switch (e.Name)
         {
-            // case "attract": // TODO- handle n attractManager
             // 7 levels
+            // play videos
+            // control small monitor UI
+            // TODO - set up small PF on attract
             case "level_candy_cane_forest":
                 videoManager.playVideo(videoCandyCaneForest);
+                playfieldManager.ShowLevel(1);
                 break;
             case "level_central_park":
                 videoManager.playVideo(videoCentralPark);
+                playfieldManager.ShowLevel(2);
                 break;
             case "level_coffee":
                 videoManager.playVideo(videoCoffee);
+                playfieldManager.ShowLevel(3);
                 break;
             case "level_gimbels":
                 videoManager.playVideo(videoGimbels);
+                playfieldManager.ShowLevel(4);
                 break;
             case "level_gumdrop":
                 videoManager.playVideo(videoGumdrop);
+                playfieldManager.ShowLevel(5);
                 break;
             case "level_lincoln_tunnel":
                 videoManager.playVideo(videoLincolnTunnel);
+                playfieldManager.ShowLevel(6);
                 break;
             case "level_nutcracker":
                 videoManager.playVideo(videoNutcracker);
+                playfieldManager.ShowLevel(7);
                 break;
             // sub-levels
             case "ball_lock":
+                playfieldManager.ShowLevel(8);
                 break;
             case "end_of_ball_bonus":
                 break;
             case "jackpot":
+                playfieldManager.ShowLevel(9);
                 break;
             case "jet_bonus":
                 break;
@@ -80,14 +93,17 @@ public class ModeManager : MonoBehaviour
                 videoManager.playVideo(videoOmg);
                 break;
             case "plunger_skill_shot":
+                playfieldManager.ShowLevel(10);
                 break;
             case "ramp_shot":
+                playfieldManager.ShowLevel(11);
                 break;
             case "singing":
                 videoManager.playVideo(videoSinging);
                 break;
             case "someone_special":
                 videoManager.playVideo(videoSomeoneSpecial);
+                playfieldManager.ShowLevel(12);
                 break;
             
         }
--- Assets/PlayfieldManager.cs
+++ Assets/PlayfieldManager.cs
@@ -2,17 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Controls UI on the small PF monitor
+// show/hide objects per level
 public class PlayfieldManager : MonoBehaviour
 {
+
+    public GameObject L1;
+    public GameObject L2;
+    public GameObject L3;
+    public GameObject L4;
+    public GameObject L5;
+    public GameObject L6;
+    public GameObject L7;
+    public GameObject L8;
+    public GameObject L9;
+    public GameObject L10;
+    public GameObject L11;
+    public GameObject L12;
+    public GameObject L13;
+
+    private GameObject[] screens;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        screens = new GameObject[] { L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13 };
     }
 
-    // Update is called once per frame
-    void Update()
+    // 1 -forest
+    // 2 - park
+    // 3 - coffee
+    // 4 - gimbels
+    // 5 - gumdrop
+    // 6 - nutcrackker
+    // 7 - nutcracker
+    // 8 - park
+
+    // 9 - ball lock
+    // 10 - jackpot
+    // 11 - plunger skillshot
+    // 12 - shoot ramp
+    // 13 - special
+    public void ShowLevel(int level)
     {
-        
+        hideAllExcept(level);
+        switch(level)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+        }
+    }
+
+    private void hideAllExcept(int level)
+    {
+        foreach (GameObject obj in screens)
+        {
+            obj.SetActive(false);
+        }
+
+        screens[level - 1].SetActive(true);
     }
 }
