48e9b9805b0b0cdcd4e82e30fa96db49adca5f16
BobNies <elsereturn@gmail.com>
2022-03-23 19:23:52 -0400

    remove high score

--- Assets/ModeManager.cs
+++ Assets/ModeManager.cs
@@ -55,8 +55,8 @@ public class ModeManager : MonoBehaviour
             // TODO - set up small PF on attract
             case "high_score":
                 //enable high score script
-                BcpLogger.Trace("bob high_score*** ");
-                this.gameObject.GetComponent<HighScoreManager>().enabled = true;
+               // BcpLogger.Trace("bob high_score*** ");
+                //this.gameObject.GetComponent<HighScoreManager>().enabled = true;
                 break;
             case "attract":
                 MasterAudio.StopPlaylist(); // just incase
@@ -159,7 +159,7 @@ public class ModeManager : MonoBehaviour
             // TODO - set up small PF on attract
             case "high_score":
                 videoManager.stopAllVideos();
-                this.gameObject.GetComponent<HighScoreManager>().enabled = false;
+               //this.gameObject.GetComponent<HighScoreManager>().enabled = false;
                 break;
         }
     }
